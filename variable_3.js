134bb6cb39e0eb0c9ec7fecbf2bc6a118bca2883
sozerodev <sozerodev@gmail.com>
2019-07-26 14:27:52 +0900

    변수(variable)에 대해서 공부. (변수선언/데이터타입/Boolean)

---
+++ variable_3.js
@@ -0,0 +1,25 @@
+/* Boolean 에 대해 알아보자 */
+// 불린은 참, 거짓에 대한 값밖에 출력하지 못한다.
+
+let good = true;
+let loading = false;
+
+
+// 자바스크립트에서는 null과 undifined는 둘다 '없음'을 의미한다.
+let good_2 = null;
+let loading_2 = undefined;
+
+
+// null은 없다!
+// undifined는 값이 아직 정해지지 않았다!
+
+
+
+/* for example */
+// 셜록은 친구가 없다.
+let friend = null;
+
+// 아직 범인이 누구인지 확정되지 않았다.
+let criminal = undefined;
+console.log(criminal)
+
